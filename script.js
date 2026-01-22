9c22a1cbe452a0922dfc3b4948196f63f1a0b6d4
navjot0612 <navjotsinghjuneja8@gmail.com>
2026-01-22 10:38:15 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let currentPage = 1;
-
-function nextPage() {
-    document.getElementById(page${currentPage}).classList.add("hidden");
-    currentPage++;
-    document.getElementById(page${currentPage}).classList.remove("hidden");
-}
